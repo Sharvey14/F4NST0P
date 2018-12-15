57ece02d9289a4fa7dd7f823f7b5c3bd70131cb2
Sharvey14 <brownieb2411@gmail.com>
2018-12-15 15:43:06 -0800

    Hi Hi :)

--- app/src/main/java/com/shannon/student/f4nst0p/MainActivity.java
+++ app/src/main/java/com/shannon/student/f4nst0p/MainActivity.java
@@ -2,12 +2,29 @@ package com.shannon.student.f4nst0p;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class MainActivity extends Activity {
+
+    //variables go here
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void submitOrder (View view) {
+        CheckBox krustyHeadset = (CheckBox) findViewById(R.id.krusty_headset);
+        boolean hasHeadset = krustyHeadset.isChecked();
+        CheckBox homerPs4Skin = (CheckBox) findViewById(R.id.homer_ps4_skin);
+        boolean hasHomer = homerPs4Skin.isChecked();
+        CheckBox homerDonutSkin = (CheckBox) findViewById(R.id.homer_donut_skin);
+        boolean hasDonut = homerDonutSkin.isChecked();
+        CheckBox HomerPsvrSkin = (CheckBox) findViewById(R.id.homer_psvr_skin);
+        boolean hasPsvr = HomerPsvrSkin.isChecked();
+    }
+
 }
